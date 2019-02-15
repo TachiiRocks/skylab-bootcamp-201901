527a1fe1bf269e65e8cba76b0f44c6409496e837
Tachi <jrfluna8@gmail.com>
2019-02-15 23:34:05 +0100

    initial commit

---
+++ staff/tachi-fernandez/spotify-web-app/index.js
@@ -0,0 +1,199 @@
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+// const FileStore = require('session-file-store')(session)
+const logicFactory = require('./src/logic-factory')
+
+const { env: { PORT }, argv: [, , port = PORT || 8080] } = process
+
+const app = express()
+
+app.use(session({
+    secret: 'a secret phrase',
+    resave: true,
+    saveUninitialized: true,
+    // store: new FileStore({
+    //     path: './.sessions'
+    // })
+}))
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+app.set('views', './src/components')
+
+const formBodyParser = bodyParser.urlencoded({ extended: false })
+
+function pullFeedback(req) {
+    const { session: { feedback } } = req
+
+    req.session.feedback = null
+
+    return feedback
+}
+
+function renderPage(content) {
+    return `<html>
+<head>
+    <title>HELLO WORLD!</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="main">
+    <h1>HELLO WORLD! 🤡</h1>
+    ${content}
+</body>
+</html>`
+}
+
+app.get('/', (req, res) => {
+    res.render('landing')
+})
+
+app.get('/register', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/search')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('register', { feedback })
+    }
+})
+
+app.post('/register', formBodyParser, (req, res) => {
+    const { body: { name, surname, email, password, passwordConfirm } } = req
+
+    const logic = logicFactory.create(req)
+
+    try {
+        logic.registerUser(name, surname, email, password, passwordConfirm)
+            .then(() => res.render('register-confirm' , { email }))
+            .catch(({ message }) => {
+                req.session.feedback = message
+
+                res.redirect('/register')
+            })
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/register')
+    }
+})
+
+app.get('/login', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/search')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('login', { feedback })
+    }
+})
+
+app.post('/login', formBodyParser, (req, res) => {
+    const { body: { email, password } } = req
+
+    const logic = logicFactory.create(req)
+
+    try {
+        logic.logInUser(email, password)
+            .then(() => res.redirect('/search'))
+            .catch(({ message }) => {
+                req.session.feedback = message
+
+                res.redirect('/login')
+            })
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/login')
+    }
+})
+
+app.get('/search', (req, res) => {
+    try {
+        const { session: { feedback } } = req
+
+        const logic = logicFactory.create(req)
+
+        if (logic.isUserLoggedIn)
+            logic.retrieveUser()
+                .then( () => res.render('search'))
+                .catch(({ message }) => {
+                    req.session.feedback = message
+
+                    res.redirect('/search')
+                })
+        else res.redirect('/login')
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/search')
+    }
+})
+
+app.post('/search',formBodyParser, (req,res) => {
+    const { body:{ query } } = req
+
+    const logic = logicFactory.create(req)
+
+    try{
+        logic.searchArtists(query)
+            .then(items => console.log(items)
+            // res.redirect('/artists')        
+            ).catch(({ message }) => {
+                req.session.feedback = message
+
+                res.redirect('/search')
+            })
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/search')
+    }
+})
+
+// app.get('/artists', (req, res) => {
+//     try {
+//         const { session: { feedback } } = req
+
+//         const logic = logicFactory.create(req)
+
+//         if (logic.isUserLoggedIn)
+//             logic.retrieveUser()
+//                 .then( () => res.render('artists'))
+//                 .catch(({ message }) => {
+//                     req.session.feedback = message
+
+//                     res.redirect('/search')
+//                 })
+//         else res.redirect('/login')
+//     } catch ({ message }) {
+//         req.session.feedback = message
+
+//         res.redirect('/search')
+//     }
+// })
+
+
+app.post('/logout', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    logic.logOutUser()
+
+    res.redirect('/')
+})
+
+app.get('*', (req, res) => res.send(404, renderPage(`<section class="not-found">
+        <h2>NOT FOUND</h2>
+
+        Go <a href="/">Search</a>
+    </section>`)))
+
+
+app.listen(port, () => console.log(`server running on port ${port}`))
